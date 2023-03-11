4e5e2d16ad522f3e61d94d02fdd00f3606e7e201
Enoxs <enoxss@gmail.com>
2023-03-11 13:25:38 +0200

    Update dropdownstexts.py

--- dropdownstexts/dropdownstexts.py
+++ dropdownstexts/dropdownstexts.py
@@ -1,37 +1,40 @@
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_components import create_select, create_select_option, create_actionrow
+from discord_slash.model import SlashCommandOptionType
+
 import discord
 from discord.ext import commands
-from discord_components import DiscordComponents, Select, SelectOption
 
 
-class RolesDropdown(commands.Cog):
+class RoleSelector(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        DiscordComponents(bot)
 
-    @commands.command()
-    async def roles(self, ctx):
+    @cog_ext.cog_slash(
+        name="roles",
+        description="Select roles from a dropdown menu",
+        options=[
+            {
+                "name": "roles",
+                "description": "Select the roles you want to assign",
+                "type": SlashCommandOptionType.STRING,
+                "required": True,
+                "choices": [
+                    create_select_option(role.name, role.id, emoji=str(role.emoji)) for role in your_roles_here
+                ],
+                "multiple": True,
+            }
+        ],
+    )
+    async def role_selector(self, ctx: SlashContext, roles):
+        member = ctx.author
         guild = ctx.guild
-        options = [
-            SelectOption(role.name, str(role.id), emoji=role.mention)
-            for role in guild.roles
-        ]
-        dropdown = Select(
-            placeholder="Choose your roles",
-            options=options,
-            max_values=len(options),
+        roles_to_add = [discord.Object(id=int(r)) for r in roles]
+        await member.add_roles(*roles_to_add, reason="Selected via dropdown menu")
+        await ctx.send(
+            f"Assigned the following roles to {member.display_name}: {', '.join([r.name for r in roles_to_add])}",
+            hidden=True,
         )
-        await ctx.send("Choose your roles:", components=[dropdown])
-
-        def check(interaction):
-            return interaction.message.id == ctx.message.id
-
-        interaction = await self.bot.wait_for("select_option", check=check)
-        roles = [
-            guild.get_role(int(value))
-            for value in interaction.values
-        ]
-        await interaction.respond(content=f"You have chosen the following roles: {', '.join([role.mention for role in roles])}", type=7)
-
 
 def setup(bot):
-    bot.add_cog(RolesDropdown(bot))
+    bot.add_cog(RoleSelector(bot))
